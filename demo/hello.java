4b73e5ffb53bc748766d1e34e04e88c5989dfb0b
Anurag Rastogi <>
2024-03-13 17:39:09 +0000

    first commit

---
+++ demo/hello.java
@@ -0,0 +1,17 @@
+package demo;
+
+/**
+ * hello
+ */
+public class hello {
+
+    public static void main(String s[])
+    {
+
+        System.out.println("Helpp");
+        System.out.println("Helpp");
+        
+        }
+
+    
+}
